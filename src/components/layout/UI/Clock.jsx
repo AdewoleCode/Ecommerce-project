751f87d89c22b69b7e4172fe0bb2d3b786d8cd03
ademola <adeadewole1712@gmail.com>
2022-10-12 15:15:52 +0100

    adding the clock function

--- src/components/layout/UI/Clock.jsx
+++ src/components/layout/UI/Clock.jsx
@@ -1,12 +1,52 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import '../../../styles/Clock.css'
 
 const Clock = () => {
+    let [days,setDays]= useState()
+    let [hours,setHours]= useState()
+    let [minutes,setMinutes]= useState()
+    let [seconds,setSeconds]= useState()
+
+
+    let intervals;
+
+    const countDown = () => {
+        const destination = new Date('oct 23, 2022').getTime()
+
+        intervals = setInterval(()=> {
+
+            const now = new Date().getTime()
+            const different = destination - now
+
+            const days = Math.floor(different / (1000 * 60 * 60 * 24))
+            const hours = Math.floor(different % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
+            const minutes = Math.floor(different % (1000 * 60 * 60) / (1000 * 60 ))
+            const seconds = Math.floor(different % (1000 * 60 ) / 1000)
+
+            if (destination < 0) clearInterval(intervals.current)
+            else{
+                setDays(days)
+                setHours(hours)
+                setMinutes(minutes)
+                setSeconds(seconds)
+            }
+        })
+    }
+
+    useEffect(() => {
+        countDown()
+    }, [])
+
+
+
+
+
+
     return (
         <div className='clock__wrapper'>
             <div className="clock__data">
                 <div className="center">
-                    <h1>10</h1>
+                    <h1>{days}</h1>
                     <h5>Days</h5>
                 </div>
                 <span>:</span>
@@ -14,7 +54,7 @@ const Clock = () => {
 
             <div className="clock__data">
                 <div className="center">
-                    <h1>10</h1>
+                    <h1>{hours}</h1>
                     <h5>Hours</h5>
                 </div>
                 <span>:</span>
@@ -23,7 +63,7 @@ const Clock = () => {
 
             <div className="clock__data">
                 <div className="center">
-                    <h1>10</h1>
+                    <h1>{minutes}</h1>
                     <h5>Minutes</h5>
                 </div>
                 <span>:</span>
@@ -32,7 +72,7 @@ const Clock = () => {
 
             <div className="clock__data">
                 <div className="center">
-                    <h1>10</h1>
+                    <h1>{seconds}</h1>
                     <h5>Seconds</h5>
                 </div>
                 <span>:</span>
