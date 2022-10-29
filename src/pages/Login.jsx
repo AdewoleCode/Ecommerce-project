4c1a59298cfaf960b55119be35f6096d49552e46
ademola <adeadewole1712@gmail.com>
2022-10-29 15:14:49 +0100

    signup pages and checkout page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,29 @@
 
 
-
-
 import React from 'react'
+
+import { Link } from 'react-router-dom'
+
+import '../pages/Login.css'
 
 const Login = () => {
   return (
-    <div>Logissqsqsqssqssqqdqdqn
-      <h1>hello</h1>
+    <div className="login">
+      <h3>Login</h3>
+      <form action="">
+        <div className="name">
+          <input type="text" placeholder='Enter Your Email...' />
+        </div>
+        <div className="email">
+          <input type="password" placeholder='Enter Your Password...' />
+        </div>
+
+        <button className='login__btn'>Login</button>
+
+        <h5>Dont have an account yet?  <Link to='signup'><span >Create an account</span> </Link> </h5>
+
+
+      </form>
     </div>
   )
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,9 +3,30 @@
 
 import React from 'react'
 
+
+import { Link } from 'react-router-dom'
+
+import '../pages/SignUp.css'
+
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className="signup">
+      <h3>Login</h3>
+      <form action="">
+        <div className="name">
+          <input type="text" placeholder='Enter Your Email...' />
+        </div>
+        <div className="email">
+          <input type="password" placeholder='Enter Your Password...' />
+        </div>
+
+        <button className='login__btn'>Sign Up</button>
+
+        <h5>Already have an account?  <Link to='login'><span >Click to login</span> </Link> </h5>
+
+
+      </form>
+    </div>
   )
 }
 
